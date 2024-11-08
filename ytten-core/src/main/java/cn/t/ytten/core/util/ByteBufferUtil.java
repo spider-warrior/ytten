52226aae595ad1effbbc73096a8c5aa56969fde0
36296 <362961910@qq.com>
2024-11-08 17:26:37 +0800

    init

--- ytten-core/src/main/java/cn/t/ytten/core/util/ByteBufferUtil.java
+++ ytten-core/src/main/java/cn/t/ytten/core/util/ByteBufferUtil.java
@@ -4,12 +4,14 @@ import java.nio.ByteBuffer;
 
 public class ByteBufferUtil {
 
+    private static final int defaultCapacity = 1024 * 1024;
+//    private static final int defaultCapacity = 2;
     private static final ThreadLocal<ByteBuffer> threadLocal = new ThreadLocal<>();
 
     public static ByteBuffer allocate() {
         ByteBuffer buffer = threadLocal.get();
         if (buffer == null) {
-            buffer = ByteBuffer.allocateDirect(1024 * 1024);
+            buffer = ByteBuffer.allocateDirect(defaultCapacity);
             threadLocal.set(buffer);
         } else {
             buffer.clear();
