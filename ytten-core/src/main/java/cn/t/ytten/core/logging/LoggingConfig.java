0468b6f38623828f3767f8f41098075a0f9c8e8e
36296 <362961910@qq.com>
2024-11-11 19:48:50 +0800

    init

--- ytten-core/src/main/java/cn/t/ytten/core/logging/LoggingConfig.java
+++ ytten-core/src/main/java/cn/t/ytten/core/logging/LoggingConfig.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 public class LoggingConfig {
     static {
         // 获取根Logger
-        Logger rootLogger = LogManager.getLogManager().getLogger("");
+        Logger rootLogger = LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
         // 移除默认的处理器
         rootLogger.setUseParentHandlers(false);
         rootLogger.addHandler(new StandardConsoleHandler());
